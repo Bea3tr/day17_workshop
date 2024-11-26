ebbbd885e89b48cc552616b9c578d932f03fac64
Beatrice <beatricesim31@gmail.com>
2024-11-26 12:04:23 +0800

    spring-boot template

---
+++ src/test/java/ssf/day17_workshop/Day17WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package ssf.day17_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day17WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
